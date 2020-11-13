d3d3ec71abae6b83d69b34e7d2f7cc602aa69e64
Dorumuk <haulinef@gmail.com>
2020-11-13 17:58:21 +0900

    T2 학생 화면 UI 작업 및 기능(drag n drop) 약간 구현

--- src/B_ls_writing/student/s_question/_quiz_item.tsx
+++ src/B_ls_writing/student/s_question/_quiz_item.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Draggable from 'react-draggable';
 
 import { QPROG } from '../s_store';
 import * as common from '../../common';
@@ -18,9 +19,28 @@ class QuizItem extends React.Component<IQuizItem> {
 	private _onChoice = (choice: number) => {
 		this.props.onChoice(this.props.idx, choice);
 	}
+	state = {
+		activeDrags: 0,
+		deltaPosition: {
+		x: 0, y: 0
+		},
+		controlledPosition: {
+		x: -400, y: 200
+		}
+	};
+	
+	onStart = () => {
+		this.setState({activeDrags: ++this.state.activeDrags});
+	};
+	
+	onStop = () => {
+		this.setState({activeDrags: --this.state.activeDrags});
+	};
+	
 	public render() {
 		const {view, idx, choice, confirm_normal, questionProg} = this.props;
-		return (
+		const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
+		return (	
 			<>
 				<div className="quiz"><div>
 					<WrapTextNew 
@@ -29,16 +49,17 @@ class QuizItem extends React.Component<IQuizItem> {
 						{confirm_normal.directive.kor}
 					</WrapTextNew>
 				</div></div>
-				<div className="choice">
-					{/* 예시:  <img src={App.data_url + data.item1.img} draggable={false}/> */}
-					<img className="image" src={App.data_url + confirm_normal.item1.img}/>
-					<img className="image" src={App.data_url + confirm_normal.item2.img}/>
-					<img className="image" src={App.data_url + confirm_normal.item3.img}/>
-				</div>
-				<div className="box">
-					<div className="num" draggable={true}><text>1</text></div>
-					<div className="num" draggable={true}><text>2</text></div>
-					<div className="num" draggable={true}><text>3</text></div>
+				<div className="quiz_box">
+					<div className="choice">
+						<img className="image" src={App.data_url + confirm_normal.item1.img}/>
+						<img className="image" src={App.data_url + confirm_normal.item2.img}/>
+						<img className="image" src={App.data_url + confirm_normal.item3.img}/>
+					</div>
+					<div className="place">
+						<Draggable bounds="parent" {...dragHandlers}>
+							<div className="box">1</div>
+						</Draggable>
+					</div>
 				</div>
 			</>		
 		);
